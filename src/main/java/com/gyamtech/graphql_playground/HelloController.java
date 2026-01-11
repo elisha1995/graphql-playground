11bc73ddf696fd89370b4ad4a0b60424071ff2f0
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2026-01-11 09:31:23 +0000

    Add `random` query to HelloController and update GraphQL schema

--- src/main/java/com/gyamtech/graphql_playground/HelloController.java
+++ src/main/java/com/gyamtech/graphql_playground/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
 public class HelloController {
 
@@ -16,5 +18,10 @@ public class HelloController {
     @QueryMapping
     public Mono<String> sayHelloTo(@Argument String name) {
         return Mono.fromSupplier(() -> "Hello " + name);
+    }
+
+    @QueryMapping
+    public Mono<Integer> random() {
+        return Mono.just(ThreadLocalRandom.current().nextInt(1, 100));
     }
 }
